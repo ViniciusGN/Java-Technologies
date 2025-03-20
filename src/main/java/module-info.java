1dffb1845c06e22efc2094bbc322424a44f302a1
ViniciusGN <170115437@aluno.unb.br>
2025-03-20 20:56:48 +0100

    Organizando folder

--- src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.example.imageviewer {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires java.desktop;
-
-    opens com.example.imageviewer.image_viewer to javafx.fxml, javafx.graphics;
-    exports com.example.imageviewer.image_viewer;
-}
